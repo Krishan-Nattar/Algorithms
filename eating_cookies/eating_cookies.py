dca51140ee8cd8d125f55d7b3df223a570231ecd
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-18 20:32:52 -0700

    eating cookies complete

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,19 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  count = 0
+  if n == 0:
+    return 1
+  elif n ==1:
+    return 1
+  elif n==2:
+    return 2
+  else:
+    arr = [1,1,2]
+    for number in range(3, n+1):
+      count = arr[-3]+arr[-2]+arr[-1]
+      arr.append(count)
+  return count
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -14,3 +26,5 @@ if __name__ == "__main__":
     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
     print('Usage: eating_cookies.py [num_cookies]')
+
+    
