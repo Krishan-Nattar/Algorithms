145c30bafb492dfcecda357a04782c6769987e10
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-18 23:16:14 -0700

    Started RPS game

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,24 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  return_list = []
+  one_list = ['rock','paper','scissors']
 
+  # while True:
+  for num in range(0,n**3):
+    # for item in one_list:
+    #   return_list[num].append(item)
+    return_list.append([])
+
+    # n -= 1
+    # if n == 0:
+      # break
+
+  print(len(return_list))
+
+
+  # pass 
+rock_paper_scissors(3)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -12,3 +28,8 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
+#3 - 27
+#2 - 9
+#1 3
+
