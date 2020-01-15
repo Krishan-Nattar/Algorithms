bb038e3dbd171f8adb526d570e693e60c7cd2980
Krishan Nattar <krishan.nattar@gmail.com>
2020-01-15 16:56:25 -0500

    making eating cookies better

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,15 +6,21 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
+
+  # if cookies is negative, there will be 0 ways of eating cookies
   count = 0
-  if n == 0:
-    return 1
-  elif n ==1:
+
+  # only 1 way of eating 0 cookies.
+  # only 1 way of eating 1 cookie
+  if n == 0 or n ==1: 
     return 1
   elif n==2:
     return 2
   else:
+    # arr  = ways of eating 0, 1, and 2 cookies
     arr = [1,1,2]
+
+    # Every subsequent cookie will have a total amount of ways to eat it EQUAL to the sum of the count of the 3 below it.
     for number in range(3, n+1):
       count = arr[-3]+arr[-2]+arr[-1]
       arr.append(count)
