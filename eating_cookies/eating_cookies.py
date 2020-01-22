9c180e69d443ccb8a4527875aad10c9e0becaff0
Krishan Nattar <krishan.nattar@gmail.com>
2020-01-22 18:33:55 -0500

    cookies down to 3 lines!

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,24 +7,11 @@ import sys
 # recursive solution
 def eating_cookies(n, cache=None):
 
-  # if cookies is negative, there will be 0 ways of eating cookies
-  count = 0
-
-  # only 1 way of eating 0 cookies.
-  # only 1 way of eating 1 cookie
-  if n == 0 or n ==1: 
-    return 1
-  elif n==2:
-    return 2
-  else:
-    # arr  = ways of eating 0, 1, and 2 cookies
-    arr = [1,1,2]
-
+  arr = [1,1,2] + [0] * (n-2)
     # Every subsequent cookie will have a total amount of ways to eat it EQUAL to the sum of the count of the 3 below it.
-    for number in range(3, n+1):
-      count = arr[-3]+arr[-2]+arr[-1]
-      arr.append(count)
-  return count
+  for number in range(3, n+1):
+    arr[number] = arr[number-3]+arr[number-2]+arr[number-1]
+  return arr[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
